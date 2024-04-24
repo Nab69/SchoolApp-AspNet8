20c8bfc78e47ced9fd939476d6e7299264f03b8b
Nab <17692533+Nab69@users.noreply.github.com>
2024-04-24 15:14:17 +0200

    attributes

---
+++ SchoolWeb/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWeb.Controllers
+{
+    //[Route("Stud")]
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("Info/{studentid}")]
+        public IActionResult Details(int studentid)
+        {
+            return View();
+        }
+
+        //[HttpGet("Add")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
