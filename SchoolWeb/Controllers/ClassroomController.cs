2760b3fc78a906f0ae8fc86bfecd468597190ef5
Nab <17692533+Nab69@users.noreply.github.com>
2024-04-24 16:00:07 +0200

    crud

---
+++ SchoolWeb/Controllers/ClassroomController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace SchoolWeb.Controllers
+{
+    public class ClassroomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok();
+        }
+
+        public IActionResult Create()
+        {
+            return Content("création", "text/plain", Encoding.UTF8);
+        }
+
+        [HttpGet("Classroom/Photo/{photoid}")]
+        public IActionResult Photo(int photoid, [FromServices] IWebHostEnvironment environment)
+        {
+            //string path = Path.Join(environment.ContentRootPath, "photos", $"teacher_{photoid}.png");
+            string path = Path.Join(environment.ContentRootPath, "photos", "teacher-logo.png");
+
+            return PhysicalFile(path, "image/png", "teacher.png");
+        }
+    }
+}
