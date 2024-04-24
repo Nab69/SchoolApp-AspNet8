f05a27b47492c6982887d478126eb2c18dd18469
Nab <17692533+Nab69@users.noreply.github.com>
2024-04-24 15:15:37 +0200

    middleware

---
+++ SchoolWeb/Middlewares/SchoolFilesMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using System.Threading.Tasks;
+
+namespace SchoolWeb.Middlewares
+{
+    public static class SchoolFilesExtensions
+    {
+        public static IApplicationBuilder UseSchoolFiles(this IApplicationBuilder builder, string contentRootPath)
+        {
+            string path = Path.Join(contentRootPath, "files");
+            var fileProvider = new PhysicalFileProvider(path);
+
+            var options = new StaticFileOptions();
+            options.RequestPath = "/myfiles";
+            options.FileProvider = fileProvider;
+
+            return builder.UseStaticFiles(options);
+        }
+    }
+}
--- SchoolWeb/Program.cs
+++ SchoolWeb/Program.cs
@@ -1,3 +1,5 @@
+using SchoolWeb.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseSchoolFiles(app.Environment.ContentRootPath);
 
 app.UseRouting();
 
