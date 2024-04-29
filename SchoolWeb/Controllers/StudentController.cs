9f70ba00e7acbadef22536e3f38ff14426369441
Nab <17692533+Nab69@users.noreply.github.com>
2024-04-29 16:02:36 +0200

    studentlist

--- SchoolWeb/Controllers/StudentController.cs
+++ SchoolWeb/Controllers/StudentController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.ContentModel;
+using System.Net;
+using System;
+using DomainModel;
 
 namespace SchoolWeb.Controllers
 {
@@ -7,7 +11,29 @@ namespace SchoolWeb.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var students = new List<Student>()
+            {
+                new Student() {
+                    FirstName = "Joey",
+                    LastName = "Tribbiani",
+                    Average = 8.5,
+                    IsClassDelegate = false,
+                },
+                new Student() {
+                    FirstName = "Monica",
+                    LastName = "Geller",
+                    Average = 13.5,
+                    IsClassDelegate = true,
+                },
+                new Student() {
+                    FirstName = "Chandler",
+                    LastName = "Bing",
+                    Average = 10.0,
+                    IsClassDelegate = false,
+                },
+            };
+
+            return View("_StudentList", students);
         }
 
         //[Route("Info/{studentid}")]
