ed597a8bd4b6a1fe6b62abb8eef311398e79ac40
Nab <17692533+Nab69@users.noreply.github.com>
2024-04-30 11:37:38 +0200

    student

--- SchoolWeb/Controllers/StudentController.cs
+++ SchoolWeb/Controllers/StudentController.cs
@@ -9,43 +9,76 @@ namespace SchoolWeb.Controllers
     //[Route("Stud")]
     public class StudentController : Controller
     {
+        private static List<Student> students = new()
+        {
+            new Student() {
+                PersonID = 1,
+                FirstName = "Joey",
+                LastName = "Tribbiani",
+                Average = 8.5,
+                IsClassDelegate = false,
+            },
+            new Student() {
+                PersonID = 2,
+                FirstName = "Monica",
+                LastName = "Geller",
+                Average = 13.5,
+                IsClassDelegate = true,
+            },
+            new Student() {
+                PersonID = 3,
+                FirstName = "Chandler",
+                LastName = "Bing",
+                Average = 10.0,
+                IsClassDelegate = false,
+            },
+        };
+
         public IActionResult Index()
         {
-            var students = new List<Student>()
-            {
-                new Student() {
-                    FirstName = "Joey",
-                    LastName = "Tribbiani",
-                    Average = 8.5,
-                    IsClassDelegate = false,
-                },
-                new Student() {
-                    FirstName = "Monica",
-                    LastName = "Geller",
-                    Average = 13.5,
-                    IsClassDelegate = true,
-                },
-                new Student() {
-                    FirstName = "Chandler",
-                    LastName = "Bing",
-                    Average = 10.0,
-                    IsClassDelegate = false,
-                },
-            };
-
-            return View("_StudentList", students);
+            return View("_StudentList", StudentController.students);
         }
 
         //[Route("Info/{studentid}")]
-        public IActionResult Details(int studentid)
+        public IActionResult Details(int id)
         {
-            return View();
+            var student = StudentController.students.SingleOrDefault(s => s.PersonID == id);
+
+            if (student == null)
+                return View("Error");
+
+            return View(student);
         }
 
         //[HttpGet("Add")]
         public IActionResult Create()
         {
             return View();
+        }
+
+        public IActionResult List() 
+        {
+            return View(StudentController.students);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var student = StudentController.students.SingleOrDefault(s => s.PersonID == id);
+
+            if (student == null)
+                return View("Error");
+
+            return View(student);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = StudentController.students.SingleOrDefault(s => s.PersonID == id);
+
+            if (student == null)
+                return View("Error");
+
+            return View(student);
         }
     }
 }
