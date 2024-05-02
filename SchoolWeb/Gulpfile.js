133f2f3c7b4f59d69a022d9757d0a2aaca17ad93
Nab <17692533+Nab69@users.noreply.github.com>
2024-05-02 13:51:40 +0200

    taskrunner css

---
+++ SchoolWeb/Gulpfile.js
@@ -0,0 +1,15 @@
+﻿const gulp = require('gulp'),
+    cleanCss = require('gulp-clean-css'),
+    sass = require('gulp-sass')(require('sass'));
+
+gulp.task("sass", function () {
+    return gulp.src('Styles/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task("minifycss", () => {
+    return gulp.src('wwwroot/css/style.css')
+        .pipe(cleanCss())
+        .pipe(gulp.dest('wwwroot/css/min'))
+});
