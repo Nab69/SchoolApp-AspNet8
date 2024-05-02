a80f2a5b45e9f8cd8c28174e87addbbf8598f723
Nab <17692533+Nab69@users.noreply.github.com>
2024-05-02 14:14:27 +0200

    contexte

---
+++ Dal/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using DomainModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Classroom> Classrooms { get; set; }
+
+        #region Constructors
+        public SchoolContext()
+            : base()
+        {
+        }
+
+        public SchoolContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        #endregion
+    }
+}
