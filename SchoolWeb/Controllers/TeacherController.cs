abb40cb9b8263d6e3f58fc8bf2e79ee8163bb9f7
Nab <17692533+Nab69@users.noreply.github.com>
2024-05-02 18:46:25 +0200

    Revert "teacher async"
    This reverts commit f3dde695261ee37ecbe0eafb3517b9b6e9a15e23.

--- SchoolWeb/Controllers/TeacherController.cs
+++ SchoolWeb/Controllers/TeacherController.cs
@@ -1,102 +1,17 @@
-﻿using Dal;
-using DomainModel;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SchoolWeb.Controllers
 {
     public class TeacherController : Controller
     {
-        private readonly SchoolContext context;
-
-        public TeacherController(SchoolContext context)
-        {
-            this.context = context;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            return View(await this.context.Teachers.ToListAsync());
-        }
-
-        public async Task<IActionResult> Details(int id)
-        {
-            var teacher = await this.context.Teachers.FindAsync(id);
-
-            if (teacher == null)
-                return View("NotFound");
-
-            return View(teacher);
-        }
-
-        public IActionResult Create()
+        public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Create(Teacher teacher)
+        public IActionResult Details(int id)
         {
-            if(ModelState.IsValid)
-            {
-                this.context.Teachers.Add(teacher);
-                await this.context.SaveChangesAsync();
-
-                return RedirectToAction(nameof(Index));
-            }
-            
-            return View(teacher);
-        }
-
-        public async Task<IActionResult> Edit(int id)
-        {
-            var teacher = await this.context.Teachers.FindAsync(id);
-
-            if (teacher == null)
-                return View("NotFound");
-
-            return View(teacher);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(int id, Teacher teacher)
-        {
-            if (id != teacher.PersonID)
-                return View("Error");
-
-            if (ModelState.IsValid)
-            {
-                this.context.Teachers.Update(teacher);
-                await this.context.SaveChangesAsync();
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(teacher);
-        }
-
-        public async Task<IActionResult> Delete(int id)
-        {
-            var teacher = await this.context.Teachers.FindAsync(id);
-
-            if (teacher == null)
-                return View("NotFound");
-
-            return View(teacher);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var teacher = await this.context.Teachers.FindAsync(id);
-
-            if (teacher == null)
-                return View("NotFound");
-
-            this.context.Teachers.Remove(teacher);
-            await this.context.SaveChangesAsync();
-
-            return RedirectToAction(nameof(Index));
+            return View();
         }
     }
 }
